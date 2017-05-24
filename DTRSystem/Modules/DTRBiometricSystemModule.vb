b483e9c2ada5f7f71b54ad8babdf3a765a26656c
jptlightx005 <jptlightx005@gmail.com>
2017-05-24 13:49:12 +0800

    Added System Module

---
+++ DTRSystem/Modules/DTRBiometricSystemModule.vb
@@ -0,0 +1,9 @@
+﻿Module DTRBiometricSystemModule
+    Dim applicationPath As String
+    Sub Main()
+        applicationPath = AppDomain.CurrentDomain.BaseDirectory
+
+        Dim app As New System.Windows.Application
+        app.Run(New MainWindow)
+    End Sub
+End Module
