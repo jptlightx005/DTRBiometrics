57e6c0a16fe92356068a92b9466bbabeab65f4d7
jptlightx005 <jptlightx005@gmail.com>
2017-05-24 15:08:41 +0800

    Tested device connection (only)

--- DTRSystem/Windows/DTRBiometricWindow.xaml.vb
+++ DTRSystem/Windows/DTRBiometricWindow.xaml.vb
@@ -1,11 +1,26 @@
-﻿Public Class DTRBiometricWindow
+﻿Imports ZKFPEngXControl
+Public Class DTRBiometricWindow
+    Dim fp As ZKFPEngX
+    Public otemplate As Object
     Public Sub New()
 
         ' This call is required by the designer.
         InitializeComponent()
 
         ' Add any initialization after the InitializeComponent() call.
+        fp = New ZKFPEngX
+        
+    End Sub
 
+    Private Sub Window_Loaded(sender As Object, e As RoutedEventArgs)
+        fp.SensorIndex = 0
+        If (fp.InitEngine = 0) Then
+            imgConnected.Visibility = Windows.Visibility.Visible
+            lblConnected.Content = "Device Connected"
+        Else
+            imgConnected.Visibility = Windows.Visibility.Hidden
+            lblConnected.Content = "Device Not Connected"
+        End If
     End Sub
 
     Private Sub btnClose_Click(sender As Object, e As RoutedEventArgs) Handles btnClose.Click
@@ -13,4 +28,5 @@
         dtrMainWindow.Show()
         dtrMainWindow.Focus()
     End Sub
+
 End Class
