11c33064aded9f73276e2ed487c8b52c9d034839
jptlightx005 <jptlightx005@gmail.com>
2017-05-31 14:54:34 +0800

    Added tab control to easily switch on pages

--- DTRSystem/MainWindow.xaml.vb
+++ DTRSystem/MainWindow.xaml.vb
@@ -1,5 +1,5 @@
 ﻿Class MainWindow 
-
+    Dim registrationTab As TabItem
     Private Sub btnDTR_Click(sender As Object, e As RoutedEventArgs) Handles btnDTR.Click
         Dim dtrWindow As New DTRBiometricWindow
         dtrWindow.Show()
@@ -11,8 +11,23 @@
     End Sub
 
     Private Sub mnu_register_Click(sender As Object, e As RoutedEventArgs) Handles mnu_register.Click
-        Dim a As New RegistrationPage
-        frmPanel.Navigate(a)
-
+        If registrationTab Is Nothing Then
+            registrationTab = NewTab(New RegistrationPage, "Register")
+        End If
+        tab_panels.SelectedItem = registrationTab
     End Sub
+
+    Private Function NewTab(ByRef pg As Page, header As String) As TabItem
+        Dim tab As New TabItem
+        tab.Header = header
+
+        Dim frm As New Frame
+        frm.Content = pg
+
+        tab.Content = frm
+
+        tab_panels.Items.Add(tab)
+
+        Return tab
+    End Function
 End Class
