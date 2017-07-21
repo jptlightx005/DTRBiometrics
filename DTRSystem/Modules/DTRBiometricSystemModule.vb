6c99f4ca9b516f6b2f3525678a60f886e908cc86
jptlightx005 <jptlightx005@gmail.com>
2017-07-22 00:19:47 +0800

    Automatically copies the database file to my documents
    moved db source

--- DTRSystem/Modules/DTRBiometricSystemModule.vb
+++ DTRSystem/Modules/DTRBiometricSystemModule.vb
@@ -5,6 +5,10 @@ Imports System.IO
 
 Module DTRBiometricSystemModule
     Public applicationPath As String
+    Public myDocumentsFolder As String
+    Public dtrSystemFolder As String
+    Public dtrSystemDB As String
+
     Public dtrMainWindow As MainWindow
 
     Public tblEmployeeAdapter As New EmployeeTableAdapter
@@ -12,10 +16,26 @@ Module DTRBiometricSystemModule
 
     Sub Main()
         applicationPath = AppDomain.CurrentDomain.BaseDirectory
+        myDocumentsFolder = My.Computer.FileSystem.SpecialDirectories.MyDocuments
+        dtrSystemFolder = Path.Combine(myDocumentsFolder, "DTRBiometricSystem")
+        dtrSystemDB = Path.Combine(dtrSystemFolder, "dtr-biometric.accdb")
+        AppDomain.CurrentDomain.SetData("DataDirectory", dtrSystemFolder)
+
+        InitializeDBIfNotExist()
 
         Dim app As New System.Windows.Application
         dtrMainWindow = New MainWindow
         app.Run(dtrMainWindow)
+    End Sub
+
+    Public Sub InitializeDBIfNotExist()
+        If Not Directory.Exists(dtrSystemFolder) Then
+            Directory.CreateDirectory(dtrSystemFolder)
+        End If
+
+        If Not File.Exists(dtrSystemDB) Then
+            File.Copy(Path.Combine(applicationPath, "dtr-biometric.accdb"), dtrSystemDB)
+        End If
     End Sub
 
     Public Function DataToBitmap(data As Object) As BitmapImage
