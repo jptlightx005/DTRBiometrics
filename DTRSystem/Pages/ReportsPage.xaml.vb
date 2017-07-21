84e8e94e35925d6eb40ddf6bd73f3308802fd43a
jptlightx005 <jptlightx005@gmail.com>
2017-07-22 02:18:47 +0800

    Set data grid headers and binding

--- DTRSystem/Pages/ReportsPage.xaml.vb
+++ DTRSystem/Pages/ReportsPage.xaml.vb
@@ -7,10 +7,17 @@
 
         ' Add any initialization after the InitializeComponent() call.
         cmbEmployees.ItemsSource = tblEmployeeAdapter.GetData
+
+
     End Sub
 
     Private Sub btnSearch_Click(sender As Object, e As RoutedEventArgs) Handles btnSearch.Click
-        timeLogDataGrid.ItemsSource = tblLogAdapter.GetEmployeeTableLog(cmbEmployees.SelectedValue)
+        If fromPicker.SelectedDate Is Nothing Or toPicker.SelectedDate Is Nothing Then
+            timeLogDataGrid.ItemsSource = tblLogAdapter.GetEmployeeTableLog(cmbEmployees.SelectedValue)
+        Else
+            Dim filterExpression As String = String.Format("DateOfTheDay >= #{0}# AND DateOfTheDay <= #{1}#", fromPicker.SelectedDate, toPicker.SelectedDate)
+            timeLogDataGrid.ItemsSource = tblLogAdapter.GetEmployeeTableLog(cmbEmployees.SelectedValue).Select(filterExpression)
+        End If
 
     End Sub
 End Class
