0c2e56bacf965e456ff2ba852ae4629bb0e043be
jptlightx005 <jptlightx005@gmail.com>
2017-08-04 17:50:34 +0800

    Implemented designation in registration

--- DTRSystem/Pages/RegistrationPage.xaml.vb
+++ DTRSystem/Pages/RegistrationPage.xaml.vb
@@ -29,11 +29,13 @@ Class RegistrationPage
         employeeRow.middle_name = ""
         employeeRow.last_name = ""
         employeeRow.deptID = 0
+        employeeRow.desgID = 0
         employeeRow.work_timeb = 0
         employeeRow.work_timee = 0
         grdEmployee.DataContext = employeeRow
 
         cmbDepartment.ItemsSource = tblDeptAdapter.GetData
+        cmbDesignation.ItemsSource = tblDesgAdapter.GetData
     End Sub
     Private Sub btnEnroll_Click(sender As Object, e As RoutedEventArgs) Handles btnEnroll.Click
         Dim regFP As New RegFPWindow
